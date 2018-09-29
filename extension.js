59882636da3408aa492d147f361c1b5a61cda91a
JoseD92 <jose.daniel.duran.toro@gmail.com>
2018-09-29 15:56:37 -0400

    basic call functionality

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const cp = require('child_process')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +14,38 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.pointfree', function () {
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
+
+        const pointfree = cp.spawn('pointfree', [text]);
+        const pointful = cp.spawn('pointful', [text]);
+
+        pointfree.stdout.on('data', (data) => {
+            vscode.window.showInformationMessage('pointfree: ' + data);
+        });
+        pointfree.stderr.on('data', (data) => {
+            vscode.window.showInformationMessage('pointfree error: ' + data);
+        });
+        pointfree.on('error', (err) => {
+            vscode.window.showInformationMessage('child process "pointfree" error code: ' + err.message + "\n" + 'Path: ' + process.env['PATH']);
+        });
+
+        pointful.stdout.on('data', (data) => {
+            vscode.window.showInformationMessage('pointful: ' + data);
+        });
+        pointful.stderr.on('data', (data) => {
+            vscode.window.showInformationMessage('pointful error: ' + data);
+        });
+        pointful.on('error', (err) => {
+            vscode.window.showInformationMessage('child process "pointful" error code: ' + err.message + "\n" + 'Path: ' + process.env['PATH']);
+        });
+
     });
 
     context.subscriptions.push(disposable);
